935f19930b97fde2ed67752ea7026b5ca6492690
Cyril Lemaire <cyril.lemaire@wanadoo.fr>
2019-11-02 13:48:53 +0100

    Version 1.0.1

--- run_tests.sh
+++ run_tests.sh
@@ -13,14 +13,14 @@ RED='\033[0;31m'
 ORANGE='\033[0;33m'
 CLRCLR='\033[0m'
 
-if [ $# -ge 1 ]; then
+if [ $# -gt 0 ]; then
 	TEST_FILES=("$@")
 fi
 
 echo "Test files: ${TEST_FILES[@]}"
 
 printf "${GREEN}Compiling ${FT_PRINTF_PATH}/libftprintf.a${CLRCLR}\n"
-make -C $FT_PRINTF_PATH && cp "${FT_PRINTF_PATH}/${LIBNAME}" .
+make -C $FT_PRINTF_PATH re && cp "${FT_PRINTF_PATH}/${LIBNAME}" .
 
 printf "${GREEN}Generating C main for printf${CLRCLR}\n"
 ./generate_main.sh ${TEST_FILES[@]} > $GENERATED_C_FILE
@@ -36,9 +36,14 @@ gcc -w -o $FT_PRINTF_TESTER $GENERATED_C_FILE $LIBNAME >/dev/null
 
 function do_test
 {
+	# Yep I'm shamefully forced to run it twice because I don't know how to
+	# retrieve the exit status without losing potential '\0' in the output.
+	# (${PIPESTATUS[0]} seemed like a good plan, but failed idk why)
 	exp_output=`./${PRINTF_TESTER} $1 | cat -e`
+	./${PRINTF_TESTER} $1 >/dev/null
 	exp_return=$?
 	act_output=`./${FT_PRINTF_TESTER} $1 | cat -e`
+	./${FT_PRINTF_TESTER} $1 > /dev/null
 	act_return=$?
 	if [ \( $exp_return -ne $act_return \) -o \( "${exp_output}" != "${act_output}" \) ] ; then
 		printf "${RED}Test #%d Error! ${ORANGE}printf(%s);\n${RED}Sys: ${ORANGE}(%3d)\t\"%s\"\n${RED}You: ${ORANGE}(%3d)\t\"%s\"${CLRCLR}\n" $1 "$(cat ${TEST_FILES[@]} | head -n $1 | tail -n 1)" "${exp_return}" "${exp_output}" "${act_return}" "${act_output}"
@@ -53,7 +58,7 @@ printf "${GREEN}Running all ${NB_LINES} tests!${CLRCLR}\n"
 
 declare -i errors=0
 for ((i=1; i <= $NB_LINES; i++)); do
-	do_test $i
+	do_test $i 2>/dev/null
 	if [ $? -ne 0 ]; then
 		let errors++
 	fi
