360a0dca1b3254348fff03f1bddda63c664d0e72
Cyril Lemaire <cyril.lemaire@wanadoo.fr>
2019-11-02 11:17:34 +0100

    Version 1.0

---
+++ generate_main.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+	echo "Usage: $(basename "$0") <source files...>"
+	exit 1
+fi
+
+echo	'#include <stdio.h>'
+echo	'#include <limits.h>'
+echo	'#include <stdlib.h>'
+echo	'#include <libgen.h>'
+echo	''
+echo	'int	ft_printf(char *format, ...);'
+echo	''
+echo	'int	main(int argc, char ** argv)'
+echo	'{'
+echo	'	if (argc != 2)'
+echo	'	{'
+echo	'		printf("Usage: %s <test_number>\n", basename(argv[0]));'
+echo	'		return(-1);'
+echo	'	}'
+echo	'	switch (atoi(argv[1]))'
+echo	'	{'
+
+# Print all tests from source files as cases, ordered by line.
+# Note: I deliberately did not redirect stderr to /dev/null, generated file will
+# be corrupted if a test file can't be read!
+printf "%s" "$(cat -n "$@" | sed -E "s/\s*([0-9]+)\s*(.*)/\t\tcase \1:\n\t\t\treturn (printf(\2));/")"
+
+echo	''
+echo	'		default:'
+echo	'			return (printf("Error 418: Coffee not found!"));'
+echo	'	}'
+echo	'}'
--- run_tests.sh
+++ run_tests.sh
@@ -1,53 +1,67 @@
 #!/bin/bash
 
+# Please update these fields to match your project
 FT_PRINTF_PATH='../ft_printf_reborn'
 LIBNAME='libftprintf.a'
-PRINTF_TESTER='printf_tester.c'
-FT_PRINTF_TESTER='ft_printf_tester.c'
-RED='\033[0;31m'
-READABLE_ERR='\033[0;33m'
+
+TEST_FILES=('test'*'.txt')
+PRINTF_TESTER='printf_tester'
+FT_PRINTF_TESTER='ft_printf_tester'
+GENERATED_C_FILE='main.c'
 GREEN='\033[0;32m'
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
 CLRCLR='\033[0m'
+
+if [ $# -ge 1 ]; then
+	TEST_FILES=("$@")
+fi
+
+echo "Test files: ${TEST_FILES[@]}"
 
 printf "${GREEN}Compiling ${FT_PRINTF_PATH}/libftprintf.a${CLRCLR}\n"
 make -C $FT_PRINTF_PATH && cp "${FT_PRINTF_PATH}/${LIBNAME}" .
 
-printf "${GREEN}Compiling tester for printf${CLRCLR}\n"
-gcc -w -o printf_tester -I$FT_PRINTF_PATH/include -I$FT_PRINTF_PATH/libft -I$FT_PRINTF_PATH $PRINTF_TESTER $LIBNAME >/dev/null
+printf "${GREEN}Generating C main for printf${CLRCLR}\n"
+./generate_main.sh ${TEST_FILES[@]} > $GENERATED_C_FILE
 
-printf "${GREEN}Copying ${PRINTF_TESTER} to ${FT_PRINTF_TESTER}${CLRCLR}\n"
-sed 's/printf(/ft_printf(/g' $PRINTF_TESTER > $FT_PRINTF_TESTER
+printf "${GREEN}Compiling tester for printf${CLRCLR}\n"
+gcc -w -o $PRINTF_TESTER $GENERATED_C_FILE $LIBNAME > /dev/null
+
+printf "${GREEN}Generating C main for ft_printf${CLRCLR}\n"
+sed -i 's/(printf(/(ft_printf(/g' $GENERATED_C_FILE
 
 printf "${GREEN}Compiling tester for ft_printf${CLRCLR}\n"
-gcc -w -o ft_printf_tester -I$FT_PRINTF_PATH/include -I$FT_PRINTF_PATH/libft -I$FT_PRINTF_PATH $FT_PRINTF_TESTER $LIBNAME >/dev/null
+gcc -w -o $FT_PRINTF_TESTER $GENERATED_C_FILE $LIBNAME >/dev/null
 
 function do_test
 {
-	exp_output=`./printf_tester $1`
+	exp_output=`./${PRINTF_TESTER} $1 | cat -e`
 	exp_return=$?
-	act_output=`./ft_printf_tester $1`
+	act_output=`./${FT_PRINTF_TESTER} $1 | cat -e`
 	act_return=$?
-	if [ "${exp_return}" -eq 254 ] ; then
-		return 254
-	elif [ \( $exp_return -ne $act_return \) -o \( "${exp_output}" != "${act_output}" \) ] ; then
-		printf "${RED}TEST #%d Error!\nExpected:${CLRCLR}\n${READABLE_ERR}(%3d)\t\"%s\"${CLRCLR}\n${RED}Instead got:${CLRCLR}\n${READABLE_ERR}(%3d)\t\"%s\"${CLRCLR}\n" $1 "${exp_return}" "${exp_output}" "${act_return}" "${act_output}"
-	else
-		printf "${GREEN}Test #%d OK!${CLRCLR} [(%3d) \"%s\"]\n" $1 "${act_return}" "${act_output}"
+	if [ \( $exp_return -ne $act_return \) -o \( "${exp_output}" != "${act_output}" \) ] ; then
+		printf "${RED}Test #%d Error! ${ORANGE}printf(%s);\n${RED}Sys: ${ORANGE}(%3d)\t\"%s\"\n${RED}You: ${ORANGE}(%3d)\t\"%s\"${CLRCLR}\n" $1 "$(cat ${TEST_FILES[@]} | head -n $1 | tail -n 1)" "${exp_return}" "${exp_output}" "${act_return}" "${act_output}"
+		return 1
+#	else
+#		printf "${GREEN}Test #%d OK!${CLRCLR}\tprintf($(cat ${TEST_FILES[@]} | head -n $1 | tail -n 1)) => [(%3d) \"%s\"]\n" $1 "${act_return}" "${act_output}"
 	fi
 }
 
-if [ "$#" -lt 1 ] ; then
-	let i = 0
-	while true ; do
-		let i++
-		
-		do_test $i
-		if [ "$?" -eq 254 ] ; then
-			break
-		fi
-	done
-fi
+NB_LINES=$(wc -l ${TEST_FILES[@]} | tail -n1 | grep -Eo '^\s*[0-9]+')
+printf "${GREEN}Running all ${NB_LINES} tests!${CLRCLR}\n"
 
-for ARG in "$@" ; do
-	do_test $ARG
+declare -i errors=0
+for ((i=1; i <= $NB_LINES; i++)); do
+	do_test $i
+	if [ $? -ne 0 ]; then
+		let errors++
+	fi
 done
+
+printf "Final result: "
+if [ $errors -eq 0 ]; then
+	printf "${GREEN}Congratulations! 0 error found :D${CLRCLR}\n"
+else
+	printf "${RED}${errors} difference(s) found! :(${CLRCLR}\n"
+fi
