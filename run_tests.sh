011c29eb4387221379dc11b5f759f842a5709c60
Cyril Lemaire <cyrlemai@student.42.fr>
2019-11-04 21:19:51 +0100

    dev version, might be suboptimal

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 # Please update these fields to match your project
+
 FT_PRINTF_PATH='../ft_printf_reborn'
 LIBNAME='libftprintf.a'
 
-TEST_FILES=('test'*'.txt')
+# Settings
+
+# Change this to toggle systematic remake. Just make sure you give me an
+# up-to-date libftprintf.a!
+YOUR_MAKE_TARGETS=all
+
+# If set to true, will also show successful tests output.
+# Note please only set to commands `true` or `false`, not a value!
+SHOW_PASSED_TESTS=true
+
+# Source files tested if none is given as argument.
+DEFAULT_TEST_FILES=('test'*)
+
 PRINTF_TESTER='printf_tester'
 FT_PRINTF_TESTER='ft_printf_tester'
 GENERATED_C_FILE='main.c'
+TEMP_OUT_FILE=`mktemp`
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 ORANGE='\033[0;33m'
@@ -15,9 +29,9 @@ CLRCLR='\033[0m'
 
 if [ $# -gt 0 ]; then
 	TEST_FILES=("$@")
+else
+	TEST_FILES=$DEFAULT_TEST_FILES
 fi
-
-echo "Test files: ${TEST_FILES[@]}"
 
 printf "${GREEN}Compiling ${FT_PRINTF_PATH}/libftprintf.a${CLRCLR}\n"
 make -C $FT_PRINTF_PATH&& cp "${FT_PRINTF_PATH}/${LIBNAME}" .
@@ -34,22 +48,24 @@ sed -i .back 's/(printf(/(ft_printf(/g' $GENERATED_C_FILE
 printf "${GREEN}Compiling tester for ft_printf${CLRCLR}\n"
 gcc -w -o $FT_PRINTF_TESTER $GENERATED_C_FILE $LIBNAME >/dev/null
 
+function get_result
+{
+	./$1 $2 > $TEMP_OUT_FILE 2>&1
+	ret_val=$?
+	output=`cat -e ${TEMP_OUT_FILE}`
+	printf "(%3d) \"%s\"" "${ret_val}" "${output}"
+}
+
 function do_test
 {
-	# Yep I'm shamefully forced to run it twice because I don't know how to
-	# retrieve the exit status without losing potential '\0' in the output.
-	# (${PIPESTATUS[0]} seemed like a good plan, but failed idk why)
-	exp_output=`./${PRINTF_TESTER} $1 | cat -e`
-	./${PRINTF_TESTER} $1 >/dev/null
-	exp_return=$?
-	act_output=`./${FT_PRINTF_TESTER} $1 | cat -e`
-	./${FT_PRINTF_TESTER} $1 > /dev/null
-	act_return=$?
-	if [ \( $exp_return -ne $act_return \) -o \( "${exp_output}" != "${act_output}" \) ] ; then
-		printf "${RED}Test #%d Error! ${ORANGE}printf(%s);\n${RED}Sys: ${ORANGE}(%3d)\t\"%s\"\n${RED}You: ${ORANGE}(%3d)\t\"%s\"${CLRCLR}\n" $1 "$(cat ${TEST_FILES[@]} | head -n $1 | tail -n 1)" "${exp_return}" "${exp_output}" "${act_return}" "${act_output}"
+	expected=`get_result $PRINTF_TESTER $1`
+	actual=`get_result $FT_PRINTF_TESTER $1`
+	if [ "${expected}" != "${actual}" ]; then
+		printf "${RED}Test #%d Error! ${ORANGE}printf(%s);\n${RED}Sys: ${ORANGE}%s\n${RED}You: ${ORANGE}%s${CLRCLR}\n" $1 "$(cat ${TEST_FILES[@]} | head -n $1 | tail -n 1)" "${expected}" "${actual}"
 		return 1
-#	else
-#		printf "${GREEN}Test #%d OK!${CLRCLR}\tprintf($(cat ${TEST_FILES[@]} | head -n $1 | tail -n 1)) => [(%3d) \"%s\"]\n" $1 "${act_return}" "${act_output}"
+	elif $SHOW_PASSED_TESTS; then
+		printf "${GREEN}Test #%d OK!${CLRCLR} printf(%s) => %s\n" $1 "$(cat ${TEST_FILES[@]} | head -n $1 | tail -n 1)" "${expected}"
+		return 0
 	fi
 }
 
